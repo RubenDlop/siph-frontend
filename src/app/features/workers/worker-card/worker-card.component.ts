732ffddf5ddf2678329febe798fcb3e16e37c337
Ruben Dario Lopez <rdariolopez30@gmail.com>
2026-02-06 13:39:09 -0500

    Refactor: organize app structure (core/features/shared) and fix workers feature

---
+++ src/app/features/workers/worker-card/worker-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-worker-card',
+  imports: [],
+  templateUrl: './worker-card.component.html',
+  styleUrl: './worker-card.component.scss'
+})
+export class WorkerCardComponent {
+
+}
